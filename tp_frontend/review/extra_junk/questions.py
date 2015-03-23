8672a563c4641e2d8318fee7a6004a73cadce4e4
Gaurav Goyal <gaurav.goyal@solutionbeyond.net>
2015-03-23 13:28:47 +0530

    Fixed question insertion script

--- tp_frontend/review/extra_junk/questions.py
+++ tp_frontend/review/extra_junk/questions.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 import entity_model as em
 
-client = MongoClient("192.168.1.231",27017)
+client = MongoClient("localhost",27017)
 #client.noisy_NER.authenticate("fwadmin", "fwadmin")
 db = client['noisy_NER']
 entity_collection = db['entity']
@@ -12,23 +12,19 @@ def getQuestions():
     replyDict = {}
     
     surface_text = getAnsweredQuestions('surface_text')['result']
+    print len(surface_text)
     entity_url = getAnsweredQuestions('entity_url')['result']
+    print len(entity_url)
     questions = surface_text + entity_url
-    
-    sorted(questions, key=lambda k: k['freq'])
+    print len(questions)
+    #sorted(questions, key=lambda k: k['freq'])
     get_synonyms(questions)
 #    return questions
     
 def getAnsweredQuestions(conceptType):
     mongoReply = entity_collection.aggregate([
         {"$match":
-            {"intended_trainer":"Foodweasel_trainer",
-                "$or":[
-                    {"approved_by_trainer": {"$exists": "true"}},
-                    {"disapproved_by_trainer": {"$exists":"true"}},
-                    {"skipped_by_trainer": {"$exists":"true"}},
-                ]
-            }
+            {"intended_trainer":"Foodweasel_trainer"}
          },                 
         {
             "$unwind": "$mentioned_in"
@@ -41,14 +37,55 @@ def getAnsweredQuestions(conceptType):
             }
         },
         {"$sort" :{"freq":-1}}])
-    
+    print "here"
     return mongoReply #this retm urns a TUPLE. first element is entity_url, and 2nd element is sum of all mentioned_in
 
+# def get_synonyms(questions):
+#     """
+#     get sysnonyms of all questions
+#     """
+#     output = []
+#
+#     for ques in questions:
+#         trainers = []
+#         if '>' in ques['_id']:
+#             concept = "entity_url"
+#         else:
+#             concept = "surface_text"
+#
+#         mongoReply = entity_collection.find({
+#             concept:ques['_id'],
+#             "$and":[
+#                 {"approved_by_trainer": {"$exists": 0}},
+#                 {"disapproved_by_trainer": {"$exists":0}},
+#                 {"skipped_by_trainer": {"$exists":0}},
+#             ]
+#         })
+#         print mongoReply.count()
+#         if mongoReply.count():
+#             print "false"+ques['_id']
+#         else:
+#             data = entity_collection.find({
+#                 concept:ques['_id'],
+#                 "$or":[
+#                     {"approved_by_trainer": {"$exists": "true"}},
+#                     {"disapproved_by_trainer": {"$exists":"true"}},
+#                     {"skipped_by_trainer": {"$exists":"true"}},
+#                 ]
+#             })
+#             for d in data:
+#                 if 'approved_by_trainer' in d:
+#                     trainers = trainers + d['approved_by_trainer']
+#                 if 'disapproved_by_trainer' in d:
+#                     trainers = trainers + d['disapproved_by_trainer']
+#                 if 'skipped_by_trainer' in d:
+#                     trainers = trainers + d['skipped_by_trainer']
+#             new_c.insert({
+#                 'question': ques['_id'],
+#                 'frequency': ques['freq'],
+#                 'trainers' : list(set(trainers))
+#             })
 def get_synonyms(questions):
-    """
-    get sysnonyms of all questions
-    """
-    output = []
 
     for ques in questions:
         trainers = []
@@ -57,37 +94,29 @@ def get_synonyms(questions):
         else:
             concept = "surface_text"
 
-        mongoReply = entity_collection.find({
-            concept:ques['_id'],
-            "$and":[
-                {"approved_by_trainer": {"$exists": 0}},
-                {"disapproved_by_trainer": {"$exists":0}},
-                {"skipped_by_trainer": {"$exists":0}},
-            ]
+        mongoreply = entity_collection.find({
+            "intended_trainer":"Foodweasel_trainer",
+            concept: ques['_id'],
         })
-        print mongoReply.count()
-        if mongoReply.count():
-            print "false"+ques['_id']
-        else:
-            data = entity_collection.find({
-                concept:ques['_id'],
-                "$or":[
-                    {"approved_by_trainer": {"$exists": "true"}},
-                    {"disapproved_by_trainer": {"$exists":"true"}},
-                    {"skipped_by_trainer": {"$exists":"true"}},
-                ]
-            })
-            for d in data:
-                if 'approved_by_trainer' in d:
-                    trainers = trainers + d['approved_by_trainer']
-                if 'disapproved_by_trainer' in d:
-                    trainers = trainers + d['disapproved_by_trainer']
-                if 'skipped_by_trainer' in d:
-                    trainers = trainers + d['skipped_by_trainer']
+        is_ques = True
+        for data in list(mongoreply):
+            if "approved_by_trainer" in data and len(data["approved_by_trainer"])>0:
+                trainers = trainers + data["approved_by_trainer"]
+            elif "disapproved_by_trainer" in data and len(data["disapproved_by_trainer"])>0:
+                trainers = trainers + data["disapproved_by_trainer"]
+            elif "skipped_by_trainer" in data and len(data["skipped_by_trainer"])>0:
+                trainers = trainers + data["skipped_by_trainer"]
+            else:
+                is_ques = False
+        print trainers
+        if is_ques:
             new_c.insert({
                 'question': ques['_id'],
                 'frequency': ques['freq'],
                 'trainers' : list(set(trainers))
             })
+        else:
+            print ques
+
 
 getQuestions()
