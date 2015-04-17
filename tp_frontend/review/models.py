52be0ce06b09f38b878d6914e726dfe964f9ce6c
Gaurav Goyal <gaurav.goyal@solutionbeyond.net>
2015-04-17 21:01:20 +0530

    Set display for download csv

--- tp_frontend/review/models.py
+++ tp_frontend/review/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
     outfile = models.FileField(upload_to='output/%Y/%m/%d')
+    created = models.DateTimeField(auto_now_add=True)
