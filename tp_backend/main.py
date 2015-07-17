090464a02d82916b9ac7f7b2a0f4e33150b92291
Anil Gautam <anil.gautam@ignitras.com>
2015-07-17 14:30:50 +0530

    added dummy concept digger categories

--- tp_backend/main.py
+++ tp_backend/main.py
@@ -104,9 +104,9 @@ def set_categories_and_make_request_to_concept_digger():
 #    allProperNounSeedCategories = '[["Category:Fashion_by_nationality", 4,0,1],["Category:Clothing_companies", 3,0,1]]'
 
 #Seed Dummy Categories for concept Digger 
-    allCommonNounSeedCategories = '[["Category:Food",1,0,1]]'
-    allAttributiveSeedCategories = '[["Category:shoe_Brand",1,0,1]'
-    allProperNounSeedCategories = '[["Category:Color", 1,0,1]]'
+    allCommonNounSeedCategories = '[["Category:Food",1,1,0]]'
+    allAttributiveSeedCategories = '[["Category:shoe_Brand",1,1,0]'
+    allProperNounSeedCategories = '[["Category:Color", 1,1,0]]'
 
     """
     Intialize an empty variable mongoDumpFromConceptDigger
